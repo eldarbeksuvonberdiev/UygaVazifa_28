b055e4676b1f77815bb06ed2d9cb89c6f0cd8e9a
eldarbeksuvonberdiev <eldorbek.suvonberdiev@mail.ru>
2024-10-18 07:09:49 +0500

    Model qo'shildi

--- App/Models/Crud.php
+++ App/Models/Crud.php
@@ -4,10 +4,10 @@ namespace App\Models;
 
 interface Crud{
 
-    public function conecction();
+    public function connection();
     public function all();
     public function show($id);
-    public function create();
+    public function create($data);
     public function delete($id);
     public function update($data,$id);
 }
---
+++ App/Models/Model.php
@@ -0,0 +1,72 @@
+<?php
+namespace App\Models;
+use App\Models\Crud;
+use PDO;
+
+abstract class Model implements Crud{
+
+    public $table;
+
+    public function connection()
+    {
+        return new PDO("mysql:host=localhost;dbname=product",'root','root');
+    }
+
+    public function all(){
+        $sql = "SELECT * FROM " . $this->table;
+        $result = $this->connection()->query($sql);
+        return $result->fetchAll(PDO::FETCH_OBJ);
+    }
+
+    public function show($id)
+    {
+        if (gettype((int)$id) == "integer") {
+            $sql = "SELECT * FROM " . $this->table . " WHERE id = :id";
+            $result = $this->connection()->prepare($sql);
+            $result->bindParam(":id", $id);
+            $result->execute();
+            return $result->fetch(PDO::FETCH_OBJ);
+        }
+    }
+
+    public function create($data)
+    {
+        $columns = implode(", ", array_keys($data));
+        $values = "'" . implode("','", array_values($data)) . "'";
+        $sql = "INSERT INTO " . static::$table . " ({$columns}) VALUES ({$values})";
+        $result = $this->connection()->prepare($sql)->execute();
+        if ($result) {
+            return true;
+        }
+        return false;
+    }
+
+    public function delete($id)
+    {
+        $sql = "DELETE FROM " . static::$table . " WHERE id = :id";
+        $result = $this->connection()->prepare($sql);
+        $result->bindParam(":id", $id);
+        if ($result->execute()) {
+            return true;
+        }
+        return false;
+    }
+
+    public function update($data, $id)
+    {
+        $setValue = "";
+
+        foreach ($data as $key => $value) {
+            $setValue .= "{$key} = '{$value}',";
+        }
+        $setValue = rtrim($setValue, ",");
+
+        $result = "UPDATE " . static::$table . " SET {$setValue} WHERE id = {$id}";
+        $stmt = $this->connection()->prepare($result);
+
+        return $stmt->execute();
+    }
+} 
+
+
+?>
